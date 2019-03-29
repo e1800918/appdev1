8ada0f7f5b968e38a5fb54da984b55a2390c1875
e1800918 <tqanh608@gmail.com>
2019-03-29 09:05:05 +0000

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+// This is main.c
+#include <stdio.h>
+#include "screen.h"     // for user-defined header, use double quotes
+#include <stdlib.h>
+int main(){
+    int arr[80];        // just for making a bar chart
+    for(int i=0; i<80; i++)
+        arr[i] = rand()%70 + 30;
+
+    clearScreen();
+    setColors(YELLOW, bg(WHITE));
+//  printf("Printed from main.\n");
+	barChart(arr);
+    resetColors();
+    printf("Another message\n");
+    getchar();
+}
+
